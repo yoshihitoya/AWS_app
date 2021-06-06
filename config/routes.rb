7ef0116895c5a24ee3a36b3a7ac719a4fff7336e
yoshihitoya <yoshihitoya0527@gmail.com>
2021-06-06 20:36:21 +0900

    create twitter app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
+
+  # get 'relationship/create'
+  # get 'relationship/destroy'
+  # get 'favorites/create'
+  # get 'favorites/destroy'
+  root 'tweets#index'
+
+
+  # get 'tweets/new'
+  # get 'tweets/index'
+  # get 'tweets/show'
+  # get 'tweets/create'
+  # get 'users/index'
+  # get 'users/show'
+
+
+  devise_for :users
+
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member 
+  end
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
